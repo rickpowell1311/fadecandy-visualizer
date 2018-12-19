baab718de7e9f165d13bc79694320058fe283119
Rick <rick@moneyboxapp.com>
2018-12-19 17:59:25 +0000

    Implement initial view

---
+++ src/Fadecandy.Visualizer.Client/Shared/JSInterop/Panel.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fadecandy.Visualizer.Client.Shared.JSInterop
+{
+    public static class Panel
+    {
+        public static DateTime LastReceivedOn { get; private set; }
+
+        public static double MaxFps { get; private set; }
+
+        public static bool ShouldThrottle => DateTime.UtcNow.Subtract(LastReceivedOn).TotalMilliseconds < (1d / MaxFps);
+
+        static Panel()
+        {
+            MaxFps = 60;
+        }
+
+        public static async Task Listen(double maxFps = 60)
+        {
+            if (maxFps <= 0d)
+            {
+                throw new ArgumentException("Max frames per second must be greater than 0");
+            }
+
+            MaxFps = maxFps;
+
+            await JSRuntime.Current.InvokeAsync<object>(
+                "fadecandyVisualiser.listen");
+        }
+
+        public static async Task Configure(string elementId, int height, int width)
+        {
+            // TODO: Dynamic number of pixels
+
+            await JSRuntime.Current.InvokeAsync<object>(
+                "fadecandyVisualiser.configure", elementId, height, width, 5);
+        }
+
+        public static async Task Update(int[] data)
+        {
+            await JSRuntime.Current.InvokeAsync<int[]>(
+                "fadecandyVisualiser.update", data);
+        }
+
+        [JSInvokable]
+        public static async Task OnDataReceived(string json)
+        {
+            if (!ShouldThrottle)
+            {
+                // TODO: Json Deserialize 
+                var data = json
+                    .Replace("[", string.Empty)
+                    .Replace("]", string.Empty)
+                    .Split(',')
+                    .Select(d => int.Parse(d))
+                    .ToArray();
+
+                await Update(data);
+            }
+
+            LastReceivedOn = DateTime.Now;
+        }
+    }
+}
---
+++ src/Fadecandy.Visualizer.Client/wwwroot/visualizer.js
@@ -0,0 +1,69 @@
+﻿window.fadecandyVisualiser = {
+
+    two: null,
+    circles: null,
+    isConfigured: false,
+
+    logMessage: function (message) {
+        alert(message);
+    },
+
+    configure: function (elementId, width, height, numberOfPixels) {
+
+        if (this.isConfigured) {
+            return;
+        }
+
+        var elem = document.getElementById(elementId);
+        var params = { width: width, height: height };
+        this.two = new Two(params).appendTo(elem);
+
+        // two has convenience methods to create shapes.
+        this.circles = [];
+
+        for (var i = 0; i < numberOfPixels; i++) {
+
+            // TODO: Calculate size
+            circle = two.makeCircle((i + 1) * 25, 24, 12);
+            circle.fill = 'rgb(0, 0, 0)';
+            this.circles.push(circle);
+            circle.noStroke();
+        }
+
+        this.isConfigured = true;
+        this.two.update();
+    },
+
+    update: function (data) {
+        length: null;
+
+        if (data.length / 3 >= this.circles.length) {
+            length = this.circles.length;
+        }
+        else {
+            length = data.length;
+        }
+
+        for (var i = 0; i < length * 3; i = i + 3) {
+            this.circles[i/3].fill = 'rgb(' + data[i] + ', ' + data[i + 1] + ', ' + data[i + 2] + ')';
+        }
+
+        this.two.update();
+    },
+
+    listen: function () {
+        var websocket = new WebSocket("ws://localhost:7891");
+        websocket.binaryType = "arraybuffer";
+        websocket.onmessage = function (event) {
+            var uintArray = new Uint8Array(event.data);
+
+            var data = [];
+            for (var i = 0; i < uintArray.length; i++) {
+                data[i] = uintArray[i];
+            }
+
+            DotNet.invokeMethodAsync('Fadecandy.Visualizer.Client', 'OnDataReceived', JSON.stringify(data));
+        };
+    }
+};
+
